9eba96fa66c94789cec7f516ce49aa63b271bc99
acdominguez03 <acordondominguez31@gmail.com>
2025-06-17 22:01:16 +0200

    Delete Extensions.kt

--- webviewkit/src/main/java/com/molinetenterprises/webviewkit/core/Extensions.kt
+++
@@ -1,6 +0,0 @@
-package com.molinetenterprises.webviewkit.core
-
-import androidx.compose.ui.unit.Density
-import androidx.compose.ui.unit.Dp
-
-fun Int.toDp(density: Density): Dp = with(density) { toDp() }
